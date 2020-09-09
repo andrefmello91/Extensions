4aa316e53c4818941263f15aaa63c7cda83e89c1
André Mello <andremello91@gmail.com>
2020-09-09 19:15:24 -0400

    Add matrices and vectors extensions.

---
+++ Extensions/MatricesAndVectors.cs
@@ -0,0 +1,20 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Extensions.LinearAlgebra
+{
+    public static class MatricesAndVectors
+    {
+        /// <summary>
+        /// Convert this <paramref name="array"/> to a <see cref="Vector"/>.
+        /// </summary>
+        /// <param name="array"></param>
+	    public static Vector<double> ToVector(this double[] array) => Vector<double>.Build.DenseOfArray(array);
+
+        /// <summary>
+        /// Convert this <paramref name="array"/> to a <see cref="Matrix"/>.
+        /// </summary>
+        /// <param name="array"></param>
+	    public static Matrix<double> ToMatrix(this double[,] array) => Matrix<double>.Build.DenseOfArray(array);
+    }
+}
