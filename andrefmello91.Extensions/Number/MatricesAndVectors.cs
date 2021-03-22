6c2861e37e0d84cee73194d9d960f16e4a92d8bf
andremello91 <andremello91@gmail.com>
2021-03-22 18:56:20 -0400

    Added matrix square checking.

--- andrefmello91.Extensions/Number/MatricesAndVectors.cs
+++ andrefmello91.Extensions/Number/MatricesAndVectors.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
@@ -22,5 +23,14 @@ namespace Extensions
         /// Returns true if this <paramref name="matrix"/> contains at least one <see cref="double.NaN"/>.
         /// </summary>
         public static bool ContainsNaN(this Matrix<double> matrix) => matrix.Exists(d => d.IsNaN());
+		
+        /// <summary>
+        ///		Check if this <paramref name="matrix"/> is square.
+        /// </summary>
+        /// <returns>
+        ///	True if <see cref="Matrix{T}.RowCount"/> is equal to <see cref="Matrix{T}.ColumnCount"/>.
+        /// </returns>
+        public static bool IsSquare<T>(this Matrix<T> matrix) where T : struct, IEquatable<T>, IFormattable =>
+	        matrix.RowCount == matrix.ColumnCount;
     }
 }
