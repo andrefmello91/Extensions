19c241bcf36370f7a98a8067a10a33601a8f494e
andremello91 <andremello91@gmail.com>
2021-04-01 20:02:07 -0400

    Updated unit extensions with generic types.

--- andrefmello91.Extensions/Units/UnitConverter.cs
+++ andrefmello91.Extensions/Units/UnitConverter.cs
@@ -1,4 +1,5 @@
-﻿using UnitsNet;
+﻿using System;
+using UnitsNet;
 using UnitsNet.Units;
 
 namespace andrefmello91.Extensions
@@ -6,80 +7,53 @@ namespace andrefmello91.Extensions
     public static partial class UnitExtensions
     {
 	    /// <summary>
-	    /// Convert this <paramref name="length"/> to another <see cref="LengthUnit"/>.
+	    /// Convert this <paramref name="quantity"/> to another unit.
 	    /// </summary>
-	    /// <param name="length">The value to convert.</param>
-	    /// <param name="fromUnit">The base <see cref="LengthUnit"/>.</param>
-	    /// <param name="toUnit">The target <see cref="LengthUnit"/>.</param>
-	    public static double Convert(this double length, LengthUnit fromUnit, LengthUnit toUnit = LengthUnit.Millimeter) => fromUnit == toUnit ? length : UnitConverter.Convert(length, fromUnit, toUnit);
+	    /// <param name="quantity">The value to convert.</param>
+	    /// <param name="fromUnit">The base unit.</param>
+	    /// <param name="toUnit">The target unit.</param>
+	    public static double Convert<TUnit>(this double quantity, TUnit fromUnit, TUnit toUnit) 
+		    where TUnit : Enum =>
+		    fromUnit.Equals(toUnit) 
+			    ? quantity 
+			    : UnitConverter.Convert(quantity, fromUnit, toUnit);
 
-		/// <inheritdoc cref="Convert(double,LengthUnit,LengthUnit)"/>
-        public static double Convert(this int length, LengthUnit fromUnit, LengthUnit toUnit = LengthUnit.Millimeter) => ((double) length).Convert(fromUnit, toUnit);
+		/// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
+        public static double Convert<TUnit>(this int quantity, TUnit fromUnit, TUnit toUnit)
+			where TUnit : Enum =>
+			((double) quantity).Convert(fromUnit, toUnit);
 
         /// <summary>
         /// Convert this <paramref name="length"/> in <see cref="LengthUnit.Millimeter"/> to another <see cref="LengthUnit"/>.
         /// </summary>
-        /// <inheritdoc cref="Convert(double,LengthUnit,LengthUnit)"/>
+        /// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
         public static double ConvertFromMillimeter(this double length, LengthUnit toUnit) => length.Convert(LengthUnit.Millimeter, toUnit);
 
-        /// <inheritdoc cref="ConvertFromMillimeter(double,LengthUnit)"/>
+        /// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
         public static double ConvertFromMillimeter(this int length, LengthUnit toUnit) => length.Convert(LengthUnit.Millimeter, toUnit);
-
-        /// <summary>
-        /// Convert this <paramref name="area"/> to another <see cref="AreaUnit"/>.
-        /// </summary>
-        /// <param name="area">The value to convert.</param>
-        /// <param name="fromUnit">The base <see cref="AreaUnit"/>.</param>
-        /// <param name="toUnit">The target <see cref="AreaUnit"/>.</param>
-        public static double Convert(this double area, AreaUnit fromUnit, AreaUnit toUnit = AreaUnit.SquareMillimeter) => fromUnit == toUnit ? area : UnitConverter.Convert(area, fromUnit, toUnit);
-
-        /// <inheritdoc cref="Convert(double,AreaUnit,AreaUnit)"/>
-        public static double Convert(this int area, AreaUnit fromUnit, AreaUnit toUnit = AreaUnit.SquareMillimeter) => ((double) area).Convert(fromUnit, toUnit);
 
         /// <summary>
         /// Convert this <paramref name="area"/> in <see cref="AreaUnit.SquareMillimeter"/> to another <see cref="AreaUnit"/>.
         /// </summary>
-        /// <inheritdoc cref="Convert(double,AreaUnit,AreaUnit)"/>
+        /// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
         public static double ConvertFromSquareMillimeter(this double area, AreaUnit toUnit) => area.Convert(AreaUnit.SquareMillimeter, toUnit);
 
         /// <inheritdoc cref="ConvertFromSquareMillimeter(double,AreaUnit)"/>
         public static double ConvertFromSquareMillimeter(this int area, AreaUnit toUnit) => area.Convert(AreaUnit.SquareMillimeter, toUnit);
 
         /// <summary>
-        /// Convert this <paramref name="force"/> to another <see cref="ForceUnit"/>.
-        /// </summary>
-        /// <param name="force">The value to convert.</param>
-        /// <param name="fromUnit">The base <see cref="ForceUnit"/>.</param>
-        /// <param name="toUnit">The target <see cref="ForceUnit"/>.</param>
-        public static double Convert(this double force, ForceUnit fromUnit, ForceUnit toUnit = ForceUnit.Newton) => force.IsNaN() ? 0 :  fromUnit == toUnit ? force : UnitConverter.Convert(force, fromUnit, toUnit);
-
-        /// <inheritdoc cref="Convert(double,ForceUnit,ForceUnit)"/>
-        public static double Convert(this int force, ForceUnit fromUnit, ForceUnit toUnit = ForceUnit.Newton) => ((double) force).Convert(fromUnit, toUnit);
-
-        /// <summary>
         /// Convert this <paramref name="force"/> in <see cref="ForceUnit.Newton"/> to another <see cref="ForceUnit"/>.
         /// </summary>
-        /// <inheritdoc cref="Convert(double,ForceUnit,ForceUnit)"/>
+        /// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
         public static double ConvertFromNewton(this double force, ForceUnit toUnit) => force.Convert(ForceUnit.Newton, toUnit);
 
         /// <inheritdoc cref="ConvertFromNewton(double,ForceUnit)"/>
         public static double ConvertFromNewton(this int force, ForceUnit toUnit) => force.Convert(ForceUnit.Newton, toUnit);
 
         /// <summary>
-        /// Convert this <paramref name="pressure"/> to another <see cref="PressureUnit"/>.
-        /// </summary>
-        /// <param name="pressure">The value to convert.</param>
-        /// <param name="fromUnit">The base <see cref="PressureUnit"/>.</param>
-        /// <param name="toUnit">The target <see cref="PressureUnit"/>.</param>
-        public static double Convert(this double pressure, PressureUnit fromUnit, PressureUnit toUnit = PressureUnit.Megapascal) => pressure.IsNaN() ? 0 : fromUnit == toUnit ? pressure : UnitConverter.Convert(pressure, fromUnit, toUnit);
-
-        /// <inheritdoc cref="Convert(double,PressureUnit,PressureUnit)"/>
-        public static double Convert(this int pressure, PressureUnit fromUnit, PressureUnit toUnit = PressureUnit.Megapascal) => ((double) pressure).Convert(fromUnit, toUnit);
-
-        /// <summary>
         /// Convert this <paramref name="pressure"/> in <see cref="PressureUnit.Megapascal"/> to another <see cref="PressureUnit"/>.
         /// </summary>
-        /// <inheritdoc cref="Convert(double,PressureUnit,PressureUnit)"/>
+        /// <inheritdoc cref="Convert{TUnit}(double,TUnit,TUnit)"/>
         public static double ConvertFromMPa(this double pressure, PressureUnit toUnit) => pressure.Convert(PressureUnit.Megapascal, toUnit);
 
         /// <inheritdoc cref="ConvertFromMPa(double,PressureUnit)"/>
--- andrefmello91.Extensions/Units/UnitExtensions.cs
+++ andrefmello91.Extensions/Units/UnitExtensions.cs
@@ -1,4 +1,6 @@
-﻿using MathNet.Numerics;
+﻿using System;
+using System.Globalization;
+using MathNet.Numerics;
 using UnitsNet;
 using UnitsNet.Units;
 
@@ -9,78 +11,32 @@ namespace andrefmello91.Extensions
     /// </summary>
     public static partial class UnitExtensions
     {
-        /// <summary>
-        /// Returns true if this <paramref name="length"/> is approximately equal to <paramref name="other"/>.
+	    /// <summary>
+        /// Returns true if this <paramref name="quantity"/> is approximately equal to <paramref name="other"/>.
         /// </summary>
         /// <remarks>
-        ///     If the difference between these values is smaller than <paramref name="tolerance"/>, true is returned.
-        /// </remarks>
-        /// <param name="length"></param>
-        /// <param name="other">The other <see cref="Length"/>.</param>
-        /// <param name="tolerance">The tolerance to consider <paramref name="length"/> approximately equal to <paramref name="other"/>.</param>
-	    public static bool Approx(this Length length, Length other, Length tolerance) => (length - other).Abs() <= tolerance;
+	    ///     If the difference between these values is smaller than <paramref name="tolerance"/>, true is returned.
+	    /// </remarks>
+	    /// <param name="other">The other quantity.</param>
+        /// <param name="tolerance">The tolerance to consider <paramref name="quantity"/> approximately equal to other.</param>
+	    public static bool Approx<TQuantity>(this TQuantity quantity, TQuantity other, TQuantity tolerance)
+	        where TQuantity : IQuantity =>
+				(quantity.Value - other.ToUnit(quantity.Unit).Value).Abs() <= tolerance.ToUnit(quantity.Unit).Value.Abs();
 
         /// <summary>
-        /// Returns true if this <paramref name="length"/> is approximately equal to <see cref="Length.Zero"/>.
+        ///		Returns true if this <paramref name="quantity"/> is approximately equal to zero.
         /// </summary>
-        /// <inheritdoc cref="Approx(Length,Length,Length)"/>
-        public static bool ApproxZero(this Length length, Length tolerance) => length.Approx(Length.Zero, tolerance);
+        /// <inheritdoc cref="Approx{TQuantity}"/>
+        public static bool ApproxZero<TQuantity>(this TQuantity quantity, TQuantity tolerance)
+	        where TQuantity : IQuantity =>
+				quantity.Value.Abs() <= tolerance.ToUnit(quantity.Unit).Value.Abs();
 
         /// <summary>
-        /// Returns true if this <paramref name="area"/> is approximately equal to <paramref name="other"/>.
+        ///		Returns the abbreviation of this <paramref name="unit"/>.
         /// </summary>
-        /// <param name="other">The other <see cref="Area"/>.</param>
-        /// <param name="tolerance">The tolerance to consider <paramref name="area"/> approximately equal to other.</param>
-        /// <inheritdoc cref="Approx(Length,Length,Length)"/>
-	    public static bool Approx(this Area area, Area other, Area tolerance) => (area - other).Abs() <= tolerance;
-
-        /// <summary>
-        /// Returns true if this <paramref name="area"/> is approximately equal to <see cref="Area.Zero"/>.
-        /// </summary>
-        /// <inheritdoc cref="Approx(Area, Area, Area)"/>
-        public static bool ApproxZero(this Area area, Area tolerance) => area.Approx(Area.Zero, tolerance);
-
-        /// <summary>
-        /// Returns true if this <paramref name="force"/> is approximately equal to <paramref name="other"/>.
-        /// </summary>
-        /// <param name="other">The other <see cref="Force"/>.</param>
-        /// <param name="tolerance">The tolerance to consider <paramref name="force"/> approximately equal to other.</param>
-        /// <inheritdoc cref="Approx(Length,Length,Length)"/>
-	    public static bool Approx(this Force force, Force other, Force tolerance) => (force - other).Abs() <= tolerance;
-
-        /// <summary>
-        /// Returns true if this <paramref name="force"/> is approximately equal to <see cref="Force.Zero"/>.
-        /// </summary>
-        /// <inheritdoc cref="Approx(Force, Force, Force)"/>
-        public static bool ApproxZero(this Force force, Force tolerance) => force.Approx(Force.Zero, tolerance);
-
-        /// <summary>
-        /// Returns true if this <paramref name="pressure"/> is approximately equal to <paramref name="other"/>.
-        /// </summary>
-        /// <param name="other">The other <see cref="Pressure"/>.</param>
-        /// <param name="tolerance">The tolerance to consider <paramref name="pressure"/> approximately equal to other.</param>
-        /// <inheritdoc cref="Approx(Length,Length,Length)"/>
-	    public static bool Approx(this Pressure pressure, Pressure other, Pressure tolerance) => (pressure - other).Abs() <= tolerance;
-
-        /// <summary>
-        /// Returns true if this <paramref name="pressure"/> is approximately equal to <see cref="Pressure.Zero"/>.
-        /// </summary>
-        /// <inheritdoc cref="Approx(Pressure, Pressure, Pressure)"/>
-        public static bool ApproxZero(this Pressure pressure, Pressure tolerance) => pressure.Approx(Pressure.Zero, tolerance);
-
-        /// <summary>
-        /// Returns the abbreviation of this <paramref name="unit"/>.
-        /// </summary>
-	    public static string Abbrev(this LengthUnit unit) => Length.GetAbbreviation(unit);
-
-        /// <inheritdoc cref="Abbrev(LengthUnit)"/>
-	    public static string Abbrev(this ForceUnit unit) => Force.GetAbbreviation(unit);
-
-        /// <inheritdoc cref="Abbrev(LengthUnit)"/>
-	    public static string Abbrev(this PressureUnit unit) => Pressure.GetAbbreviation(unit);
-
-        /// <inheritdoc cref="Abbrev(LengthUnit)"/>
-        public static string Abbrev(this AreaUnit unit) => Area.GetAbbreviation(unit);
+        public static string Abbrev<TUnit>(this TUnit unit)
+	        where TUnit : Enum =>
+				UnitAbbreviationsCache.Default.GetDefaultAbbreviation(unit);
 
         /// <summary>
         ///     Get the <see cref="AreaUnit"/> based on <paramref name="unit"/>.
@@ -97,93 +53,26 @@ namespace andrefmello91.Extensions
 	        };
 
         /// <summary>
-        /// Get the minimum value between two <see cref="Length"/>'s.
-        /// </summary>
-        public static Length Min(Length length1, Length length2) =>
-	        length1 <= length2
-		        ? length1
-		        : length2;
-
-        /// <summary>
-        /// Get the maximum value between two <see cref="Length"/>'s.
-        /// </summary>
-        public static Length Max(Length length1, Length length2) =>
-	        length1 >= length2
-		        ? length1
-		        : length2;
-
-        /// <summary>
-        /// Get the minimum value between two <see cref="Force"/>'s.
-        /// </summary>
-        public static Force Min(Force force1, Force force2) =>
-	        force1 <= force2
-		        ? force1
-		        : force2;
-
-        /// <summary>
-        /// Get the maximum value between two <see cref="Force"/>'s.
-        /// </summary>
-        public static Force Max(Force force1, Force force2) =>
-	        force1 >= force2
-		        ? force1
-		        : force2;
-
-        /// <summary>
-        /// Get the minimum value between two <see cref="Area"/>'s.
-        /// </summary>
-        public static Area Min(Area area1, Area area2) =>
-	        area1 <= area2
-		        ? area1
-		        : area2;
-
-        /// <summary>
-        /// Get the maximum value between two <see cref="Area"/>'s.
-        /// </summary>
-        public static Area Max(Area area1, Area area2) =>
-	        area1 >= area2
-		        ? area1
-		        : area2;
-
-        /// <summary>
-        /// Get the minimum value between two <see cref="Pressure"/>'s.
-        /// </summary>
-        public static Pressure Min(Pressure pressure1, Pressure pressure2) =>
-	        pressure1 <= pressure2
-		        ? pressure1
-		        : pressure2;
-
-        /// <summary>
-        /// Get the maximum value between two <see cref="Pressure"/>'s.
-        /// </summary>
-        public static Pressure Max(Pressure pressure1, Pressure pressure2) =>
-	        pressure1 >= pressure2
-		        ? pressure1
-		        : pressure2;
-
-        /// <summary>
-        ///     Verify if this <see cref="Length"/> is finite.
-        /// </summary>
-        /// <remarks>
-        ///     Returns Zero if the value is <see cref="double.NaN"/>, <see cref="double.PositiveInfinity"/> or <see cref="double.NegativeInfinity"/>.
-        /// </remarks>
-        public static Length ToZero(this Length length) => length.Value.IsFinite() ? length : Length.Zero;
-
-        /// <summary>
-        ///     Verify if this <see cref="Area"/> is finite.
-        /// </summary>
-        /// <inheritdoc cref="ToZero(Length)"/>
-        public static Area ToZero(this Area area) => area.Value.IsFinite() ? area : Area.Zero;
-
-        /// <summary>
         ///     Verify if this <see cref="Force"/> is finite.
         /// </summary>
-        /// <inheritdoc cref="ToZero(Length)"/>
-        public static Force ToZero(this Force force) => force.Value.IsFinite() ? force : Force.Zero;
+        /// <returns>
+        ///     Zero if the value is <see cref="double.NaN"/>, <see cref="double.PositiveInfinity"/> or <see cref="double.NegativeInfinity"/>.
+        /// </returns>
+        public static TQuantity ToZero<TQuantity>(this TQuantity force) where TQuantity : IQuantity, new() =>
+	        force.Value.IsFinite() ? force : new TQuantity();
 
         /// <summary>
-        ///     Verify if this <see cref="Pressure"/> is finite.
+        ///		Returns true if this quantity has positive value.
         /// </summary>
-        /// <inheritdoc cref="ToZero(Length)"/>
-        public static Pressure ToZero(this Pressure pressure) => pressure.Value.IsFinite() ? pressure : Pressure.Zero;
+        public static bool IsPositive<TQuantity>(this TQuantity quantity)
+	        where TQuantity : IQuantity =>
+				quantity.Value > 0;
+        
+        /// <summary>
+        ///		Returns true if this quantity has negative value.
+        /// </summary>
+        public static bool IsNegative<TQuantity>(this TQuantity quantity)
+	        where TQuantity : IQuantity =>
+				quantity.Value < 0;
     }
 }
