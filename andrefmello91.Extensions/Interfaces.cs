492b73d1fb2da1283a793207380ece1cdecc46e8
andremello91 <andremello91@gmail.com>
2021-04-30 20:23:22 -0400

    Simplified IUnitConvertible

--- andrefmello91.Extensions/Interfaces.cs
+++ andrefmello91.Extensions/Interfaces.cs
@@ -9,34 +9,32 @@ namespace andrefmello91.Extensions
 	/// <summary>
 	/// UnitConvertible interface.
 	/// </summary>
-	/// <typeparam name="T1">
-	///     The type that represents the value of the object.
-	/// </typeparam>
-	/// <typeparam name="T2">
+	/// <typeparam name="TUnit">
 	///     The enum of unit that represents the object.
-	///     <para>
-	///         <see cref="LengthUnit"/>, <see cref="ForceUnit"/>, <see cref="PressureUnit"/>, etc.
-	///     </para>
+	///		<seealso cref="LengthUnit"/>
+	///		<seealso cref="AreaUnit"/>
+	///		<seealso cref="ForceUnit"/>
+	///		<seealso cref="PressureUnit"/>
 	/// </typeparam>
-	public interface IUnitConvertible<out T1, T2>
-		where T2 : Enum
+	public interface IUnitConvertible<TUnit>
+		where TUnit : Enum
 	{
 		/// <summary>
 		/// Get/set the unit of this object.
 		/// </summary>
-		T2 Unit { get; set; }
+		TUnit Unit { get; set; }
 
 		/// <summary>
 		/// Change the unit of this object.
 		/// </summary>
 		/// <param name="unit">The unit to convert.</param>
-		void ChangeUnit(T2 unit);
+		void ChangeUnit(TUnit unit);
 
 		/// <summary>
 		/// Convert this object to another unit.
 		/// </summary>
 		/// <inheritdoc cref="ChangeUnit"/>
-		T1 Convert(T2 unit);
+		IUnitConvertible<TUnit> Convert(TUnit unit);
 	}
 
 	/// <summary>
