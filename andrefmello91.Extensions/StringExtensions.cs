4348cce54584dbde830bb1ba317f2ac3c781f8b8
andremello91 <andremello91@gmail.com>
2021-05-14 20:49:07 -0400

    Added char verifying

--- andrefmello91.Extensions/StringExtensions.cs
+++ andrefmello91.Extensions/StringExtensions.cs
@@ -14,6 +14,29 @@ namespace andrefmello91.Extensions
 		public static string SplitCamelCase(this string source) =>
 			Regex.Split(source, @"(?<!^)(?=[A-Z])").Aggregate((i, f) => $"{i} {f}");
 
+		/// <summary>
+		/// 	Verify if a <see cref="char"/> is valid to an integer number.
+		/// </summary>
+		/// <param name="c">The <see cref="char"/>.</param>
+		/// <param name="allowNegative">Allow negative signal?</param>
+		/// <returns>
+		///		True if the <see cref="char"/> is valid to an integer number.
+		/// </returns>
+		public static bool IsValidForInt(this char c, bool allowNegative = true) =>
+			char.IsDigit(c) || allowNegative && c == '-';
+
+		///  <summary>
+		///  	Verify if a <see cref="char"/> is valid to a double number.
+		///  </summary>
+		///  <param name="c">The <see cref="char"/>.</param>
+		///  <param name="decimalSeparator">The decimal separator.</param>
+		///  <param name="allowNegative">Allow negative signal?</param>
+		///  <returns>
+		/// 		True if the <see cref="char"/> is valid to a double number.
+		///  </returns>
+		public static bool IsValidForDouble(this char c, char decimalSeparator = '.', bool allowNegative = true) =>
+			char.IsDigit(c) || c == decimalSeparator || c is 'e' or 'E' || allowNegative && c == '-';
+
 		#endregion
 
 	}
